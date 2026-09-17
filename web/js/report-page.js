8970a27addc0a232f595608c93642f80ee5906ca
Gordon310 <gordon310103@gmail.com>
2026-09-17 14:15:19 +0800

    chore(deploy): bump frontend asset version to 20260917-r46 (purge a poisoned edge-cache entry for app.js)

--- web/js/report-page.js
+++ web/js/report-page.js
@@ -1,5 +1,5 @@
-import { getValidAccessToken } from "./api-client.js?v=20260917-r45";
-import { currencyForRegion, inferRegion, reportAccessState, reportCoverageState, selectPrice } from "./report-page-core.js?v=20260917-r45";
+import { getValidAccessToken } from "./api-client.js?v=20260917-r46";
+import { currencyForRegion, inferRegion, reportAccessState, reportCoverageState, selectPrice } from "./report-page-core.js?v=20260917-r46";
 
 const PRODUCT_CODE = "risk_report_single";
 const params = new URL(window.location.href).searchParams;
